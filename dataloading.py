4b35656b2bb454dd271d7362ea7aa562f1480bfe
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2020-01-30 22:01:25 -0500

    Fixed dataloading

--- dataloading.py
+++ dataloading.py
@@ -19,6 +19,7 @@ import pickle
 import numpy as np
 from os import path
 import dlutils
+import warnings
 
 
 class Dataset:
@@ -73,7 +74,7 @@ def make_datasets(cfg, folding_id, inliner_classes):
     return train_set, valid_set, test_set
 
 
-def make_dataloader(cfg, dataset, batch_size, device):
+def make_dataloader(dataset, batch_size, device):
     class BatchCollator(object):
         def __init__(self, device):
             self.device = device
@@ -85,26 +86,55 @@ def make_dataloader(cfg, dataset, batch_size, device):
                 y = torch.tensor(y, dtype=torch.int32, device=self.device)
                 return y, x
 
-    data_loader = dlutils.batch_provider(dataset, cfg.TRAIN.BATCH_SIZE, BatchCollator(device))
+    data_loader = dlutils.batch_provider(dataset, batch_size, BatchCollator(device))
     return data_loader
 
 
-def create_set_with_outlier_percentage(dataset, inliner_classes, percentage):
+def create_set_with_outlier_percentage(dataset, inliner_classes, target_percentage, concervative=True):
+    np.random.seed(0)
     dataset.shuffle()
     dataset_outlier = [x for x in dataset if x[0] not in inliner_classes]
     dataset_inliner = [x for x in dataset if x[0] in inliner_classes]
 
-    inliner_count = len(dataset_inliner)
-    outlier_count = inliner_count * percentage // (100 - percentage)
+    def increase_length(data_list, target_length):
+        repeat = (target_length + len(data_list) - 1) // len(data_list)
+        data_list = data_list * repeat
+        data_list = data_list[:target_length]
+        return data_list
 
-    if len(dataset_outlier) > outlier_count:
-        dataset_outlier = dataset_outlier[:outlier_count]
+    if not concervative:
+        inliner_count = len(dataset_inliner)
+        outlier_count = inliner_count * target_percentage // (100 - target_percentage)
+
+        if len(dataset_outlier) > outlier_count:
+            dataset_outlier = dataset_outlier[:outlier_count]
+        else:
+            outlier_count = len(dataset_outlier)
+            inliner_count = outlier_count * (100 - target_percentage) // target_percentage
+            dataset_inliner = dataset_inliner[:inliner_count]
     else:
+        inliner_count = len(dataset_inliner)
         outlier_count = len(dataset_outlier)
-        inliner_count = outlier_count * (100 - percentage) // percentage
-        dataset_inliner = dataset_inliner[:inliner_count]
+
+        current_percentage = outlier_count * 100 / (outlier_count + inliner_count)
+
+        if current_percentage < target_percentage:  # we don't have enought outliers
+            outlier_count = int(inliner_count * target_percentage / (100.0 - target_percentage))
+            dataset_outlier = increase_length(dataset_outlier, outlier_count)
+        else:  # we don't have enought inliers
+            inlier_count = int(outlier_count * (100.0 - target_percentage) / target_percentage)
+            dataset_inliner = increase_length(dataset_inliner, inlier_count)
 
     dataset = Dataset(dataset_outlier + dataset_inliner)
+
+    dataset.shuffle()
+
+    # Post checks
+    outlier_count = len([1 for x in dataset if x[0] not in inliner_classes])
+    inliner_count = len([1 for x in dataset if x[0] in inliner_classes])
+    real_percetage = outlier_count * 100.0 / (outlier_count + inliner_count)
+    assert abs(real_percetage - target_percentage) < 0.01, "Didn't create dataset with requested percentage of outliers"
+
     return dataset
 
 
